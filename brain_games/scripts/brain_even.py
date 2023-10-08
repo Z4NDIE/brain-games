4a538e1d82880b7f506a71c2557a0625694e6895
flame <sannuchel@gmail.com>
2023-10-08 20:36:52 +0300

    add updates

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import random
 import prompt
-
-
+from brain_games import cli
 from brain_games.cli import welcome_user
-
 
 def rules():
     print('Answer "yes" if number even otherwise answer "no".')
@@ -15,23 +13,32 @@ def question():
     random_number = random.randint(1, 100)
     print(f'Question: {random_number}')
 
-    global user_answer
+global user_answer
     user_answer = prompt.string('Your answer: ')
 
-    if random_number % 2 == 0:
-        if user_answer == 'yes':
-            print('Correct!')
-            question()
-        else:
-            print(f''''no' is wrong answer ;'('. Correct answer is 'yes'.
-                Let's try again, {welcome_user.name}!''')
 
-    if random_number % 2 != 0:
-        if user_answer == 'no':
+def even_or_odd():
+    for x in range(3):
+        question() 
+        if random_number % 2 == 0 and user_answer == 'yes':
             print('Correct!')
-            question()
-        else:
-            print("'yes' is wrong answer ;(. Correct answer is 'no'.")
+            continue
+        elif random_number % 2 != 0 and user_answer == 'no':
+            print('Correct!')
+            continue
+        elif random_number % 2 == 0 and user_answer == 'no':
+            print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+            print(f"Let's try again, {cli.username}!")
+            break
+        elif random_number % 2 != 0 and user_answer == 'yes':
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            print(f"Let's try again, {cli.username}!")
+            break
+        elif user_answer != 'yes' and user_answer != 'no':
+            print('This is the wrong answer ;(.')
+            print(f"Let's try again, {cli.username}!")
+            break
+
 
 
 def main():
