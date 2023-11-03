fc42f82af849a97ccd54ed9b1ba0cb66248fce15
flame <sannuchel@gmail.com>
2023-11-03 19:56:22 +0300

    is_prime function fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,7 +4,7 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(x):
-    for i in range(2, (x//2)+1):
+    for i in range(2, (x // 2) + 1):
         if x % i == 0:
             return 'no'
     return 'yes'
