3faa972567a165dc16d4e3165e44a9ee76101f58
flame <sannuchel@gmail.com>
2023-11-06 12:17:55 +0300

    make_progression function fix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,21 +3,19 @@ from random import randint
 RULES = 'What number is missing in the progression?'
 
 
-def make_progression(progression_len,
-                     step,
-                     min_num, max_num):
+def make_progression(progression_len, min_num, max_num):
     first_num = randint(min_num, max_num)
-    progression_step = randint(step)
+    progression_step = randint(min_num, max_num)
     progression = [first_num, ]
     for i in range(progression_len):
-        next_num = first_num + step
+        next_num = first_num + progression_step
         progression.append(next_num)
         first_num = next_num
     return progression
 
 
 def question_and_correct_answer():
-    progression = make_progression(10, 5, 0, 30)
+    progression = make_progression(10, 1, 10, 0, 30)
     random_index = randint(0, (len(progression) - 1))
     correct_answer = progression[random_index]
     progression[random_index] = '..'
